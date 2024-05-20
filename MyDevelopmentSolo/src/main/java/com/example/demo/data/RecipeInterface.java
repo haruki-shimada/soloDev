cbd3a15a3bfbfd81ae996e2e147022d40b44cdad
imnota <>
2024-05-20 16:41:07 +0900

    設計に基づくインターフェイスの作成

---
+++ MyDevelopmentSolo/src/main/java/com/example/demo/data/RecipeInterface.java
@@ -0,0 +1,13 @@
+package com.example.demo.data;
+
+public interface RecipeInterface {
+    public int getId();
+
+    public void setName(String name);
+
+    public String getName();
+
+    public void setProcess(String process);
+
+    public String getProcess();
+}
