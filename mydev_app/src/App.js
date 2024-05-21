002a550cce5738377909138a51f97d70191de109
imnota <>
2024-05-21 19:42:10 +0900

    検索機能の追加、およびパスの修正

--- mydev_app/src/App.js
+++ mydev_app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Recipe from './Recipe';
 import RecipeDetail from './RecipeDetail';
+import SearchBox from './SearchBox';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Recipe />} />
+            <Route exact path="/recipe" element={<Recipe />} />
             <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/recipe/search" element={<SearchBox />} />
         </Routes>
     );
 }
--- mydev_app/src/Header.js
+++ mydev_app/src/Header.js
@@ -5,14 +5,14 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <Link className="navbar-brand" to="/">レシピアプリ</Link>
+                <Link className="navbar-brand" to="/recipe">レシピアプリ</Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/recipe">レシピ一覧</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/search">レシピ検索</Link>
+                            <Link className="nav-link" to="/recipe/search">レシピ検索</Link>
                         </li>
                         {/* 他のページへのリンクを追加 */}
                     </ul>
