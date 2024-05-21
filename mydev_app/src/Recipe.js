9e20a218a2e115b3028ccf1ef1db6dadca088ff4
imnota <>
2024-05-21 14:45:46 +0900

    React側のファイル用意

---
+++ mydev_app/src/Recipe.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+
+function Recipe() {
+  const [recipe, setRecipe] = useState();
+
+  useEffect(() => {
+    console.log("call useEffect START");
+    fetch('http://localhost:8080/recipe').then(response => {
+      response.json().then(value => {
+        // ※２
+        console.log(value);
+        setRecipe(value);
+      })
+    })
+      .catch(error => {
+        console.log(error);
+        setRecipe([]);
+      });
+
+    console.log("call useEffect END");
+    return () => { };
+  }, []);
+  // レシピデータを再取得する関数
+  const fetchRecipeData = () => {
+    fetch('http://localhost:8080/recipe')
+      .then(response => response.json())
+      .then(recipe => {
+        setRecipe(recipe);
+      })
+      .catch(error => {
+        console.error('Error fetching recipe data:', error);
+        setRecipe([]);
+      });
+  }
+
+  const recipeData = recipe && recipe.map((recipe, index) => {
+    return (
+      <tr key={index}>
+        <td>{recipe.id}</td>
+        <td>{recipe.name}</td>
+        <td>{recipe.minute}</td>
+      </tr>);
+  })
+
+  return (
+    <div className="App">
+      <h3>フルーツ在庫情報</h3>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>料理名</th>
+            <th>所要時間</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipeData}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Recipe;
